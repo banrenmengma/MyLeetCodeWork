b278651022963b04d6c48187d8724c126319739a
Benjamin Brown <qgccgq1@gmail.com>
2016-02-15 23:34:43 +0800

    add AddTwoNumbers.cpp

---
+++ AddTwoNumbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode
+{
+	int val;
+	ListNode* next;
+	ListNode(int x) : val(x), next(nullptr) {}
+	
+};
+
+class Solution
+{
+public:
+	ListNode *addTwoNumbers(ListNode *l1, ListNode *l2){
+		
+	}
+};
